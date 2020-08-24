e4e6da73553ebf40e82ba423623732f4dd427586
qcf81 <qcf_81@163.com>
2020-08-24 16:52:37 +0800

    de

--- prob6.m
+++
@@ -1,9 +0,0 @@
-clc, clear, close
-
-tic
-seq = 1 : 100;
-sum1 = sum(seq.^2);
-sum2 = sum(seq)^2;
-result = sum2 - sum1;
-disp(['Answer',' ','is',' ',num2str(result)]);
-toc
--- prob7.m
+++
@@ -1,7 +0,0 @@
-clc, clear, close
-
-tic
-primeNum = primes(1e7);
-len = length(primeNum);
-disp(['Answer',' ','is',' ',num2str(primeNum(10001))]);
-toc
